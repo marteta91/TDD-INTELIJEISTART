c0701f3ba09f25cd708bc630a1f20f8f9690a0ca
marteta91 <martaguitart938@gmail.com>
2022-01-06 00:54:56 +0100

    inproces

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,20 @@
+package org.example.TDD_intelijei_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApplicationTests {
+    @Autowired
+    MockMvc mockMvc;
+    @Test
+    void loadsTheHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home")); }};
